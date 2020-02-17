20910555e7bea5bf71a01e732d231ff5f88eb6eb
chenyiik <chenyiik@outlook.com>
2020-02-17 20:17:46 +0800

    finished

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
+#!/bin/sh
 python3 weibo.py
+
+export https_proxy=http://127.0.0.1:8118
 python3 tgbot.py
+export https_proxy=
 
 head -n 3 config.json > config.json.swp
 echo '    "since_date": "'$(date +%Y-%m-%d)'",' >> config.json.swp
--- tgbot.py
+++ tgbot.py
@@ -10,30 +10,16 @@ TOKEN_FILE = 'tgbot_config/bot_token.txt'
 
 WEIBO_TEXT_LINK_PREFIXES = 'https://m.weibo.cn/detail/'
 
-text_id = 0
-chat_id = 0
+with open(JSON_FILE, 'r') as file:
+	data = json.loads(file.read())
+with open(TEXT_ID_FILE, 'r') as file:
+	text_id = int(file.read())
+with open(CHAT_ID_FILE, 'r') as file:
+	chat_id = int(file.read())
+with open(TOKEN_FILE, 'r') as file:
+	token = file.read()[:-1]
 
-def get_text_id():
-	global text_id
-	with open(TEXT_ID_FILE, 'r') as file:
-		text_id = int(file.read())
-
-def get_chat_id():
-	global chat_id
-	with open(CHAT_ID_FILE, 'r') as file:
-		chat_id = int(file.read())
-
-def get_token():
-	with open(TOKEN_FILE, 'r') as file:
-		token = file.read()[:-1]
-	return token
-
-bot = telegram.Bot(token=get_token())
-
-def set_text_id():
-	file = open(TEXT_ID_FILE, 'w')
-	file.write(str(text_id))
-	file.close()
+bot = telegram.Bot(token=token)
 
 def send_message(text):
 	weibo_text = text["text"]
@@ -41,25 +27,18 @@ def send_message(text):
 	print(weibo_text)
 	print(weibo_text_link)
 	bot.send_message(chat_id = chat_id,
-			#text = weibo_text + 
-			text = 
-			'(<a href="' + weibo_text_link + '">广州天气</a>.)',
+			text = text["text"] +
+			'(<a href="' + weibo_text_link + '">广州天气</a>)',
 			parse_mode = telegram.ParseMode.HTML,
 			disable_web_page_preview=True)
 
-def decode_data():
-	global text_id
-	with open(JSON_FILE, 'r') as file:
-		data = json.loads(file.read())
-	weibo = data['weibo']
-	current_max_text_id = text_id
-	for text in weibo:
-		if text["id"] > text_id:
-			send_message(text)
-			current_max_text_id = max(current_max_text_id, text["id"])
-	text_id = current_max_text_id
+weibo = data['weibo']
+current_max_text_id = text_id
+for text in weibo:
+	if text["id"] > text_id:
+		send_message(text)
+		current_max_text_id = max(current_max_text_id, text["id"])
+text_id = current_max_text_id
 
-get_text_id()
-get_chat_id()
-decode_data()
-#set_text_id()
+with open(TEXT_ID_FILE, 'w') as file:
+	file.write(str(text_id))
